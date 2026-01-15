4079108db482833102a80da3c7f8dffbac26deac
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-15 14:59:59 +0100

    Add Assert extensions (EncodeDecode) to avoid to duplicate codec assertions

--- tests/Lithium.Codecs.Tests/Primitives/IntegerCodecTests.cs
+++ tests/Lithium.Codecs.Tests/Primitives/IntegerCodecTests.cs
@@ -1,4 +1,3 @@
-using System.Buffers;
 using Lithium.Codecs.Primitives;
 
 namespace Lithium.Codecs.Tests.Primitives;
@@ -13,18 +12,6 @@ public sealed class IntegerCodecTests
     [InlineData(int.MinValue)]
     public void IntegerCodec_Should_EncodeAndDecode(int value)
     {
-        // Arrange
-        var codec = new IntegerCodec();
-        var bufferWriter = new ArrayBufferWriter<byte>();
-
-        // Act
-        codec.Encode(value, bufferWriter);
-        
-        var sequence = new ReadOnlySequence<byte>(bufferWriter.WrittenMemory);
-        var reader = new SequenceReader<byte>(sequence);
-        var decoded = codec.Decode(ref reader);
-
-        // Assert
-        Assert.Equal(value, decoded);
+        Assert.EncodeDecodeEqual<IntegerCodec, int>(value);
     }
 }
